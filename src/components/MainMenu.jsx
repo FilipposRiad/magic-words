50c6d3bc8225a3a1d52d0febe781169982f8e19a
Filippos <fir@fisycon.gr>
2024-02-20 15:30:54 +0100

    Updated Main Menu Screen

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -5,18 +5,18 @@ export default function MainMenu(props) {
   return (
     <div className="main-menu-container">
       <div className="logo-container">
-        <img src="./src/assets/logo/magic.png" />
-        <img src="./src/assets/logo/words.png" />
+        <img src="./src/assets/logo/logo.png" />
       </div>
       <div className="main-menu-options">
         <img
-          src="./src/assets/interface/button_menu.png"
+          src="./src/assets/main_menu/title_castle.png"
           onClick={() => props.setScreen("game")}
+          className="castle"
         />
         <img
-          src="./src/assets/interface/window_scroll.png"
-          style={{ width: "112px", height: "134px" }}
+          src="./src/assets/main_menu/title_library.png"
           onClick={() => props.setScreen("characterSelect")}
+          className="library"
         />
       </div>
     </div>
