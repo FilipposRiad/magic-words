e4bfa85cf2301e56293fb5da4fe421451b0b0307
Filippos <fir@fisycon.gr>
2024-02-20 15:36:21 +0100

    Cleanup Main Menu Code

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -4,21 +4,17 @@ import "./MainMenu.css";
 export default function MainMenu(props) {
   return (
     <div className="main-menu-container">
-      <div className="logo-container">
-        <img src="./src/assets/logo/logo.png" />
-      </div>
-      <div className="main-menu-options">
-        <img
-          src="./src/assets/main_menu/title_castle.png"
-          onClick={() => props.setScreen("game")}
-          className="castle"
-        />
-        <img
-          src="./src/assets/main_menu/title_library.png"
-          onClick={() => props.setScreen("characterSelect")}
-          className="library"
-        />
-      </div>
+      <img src="./src/assets/logo/logo.png" />
+      <img
+        src="./src/assets/main_menu/title_castle.png"
+        onClick={() => props.setScreen("game")}
+        className="castle"
+      />
+      <img
+        src="./src/assets/main_menu/title_library.png"
+        onClick={() => props.setScreen("characterSelect")}
+        className="library"
+      />
     </div>
   );
 }
