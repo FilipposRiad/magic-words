d2d68b6410a13b51c9187a9e46e22673e1f3930a
Filippos <fir@fisycon.gr>
2024-06-05 10:07:06 +0200

    Remove redundant comments

--- src/App.jsx
+++ src/App.jsx
@@ -5,40 +5,8 @@ import MainMenu from "./components/MainMenu";
 import Game from "./components/Game";
 import GameMemory from "./components/GameMemory";
 import CharacterSelect from "./components/CharacterSelect";
-import Library from "./components/Library";
+// import Library from "./components/Library";
 import Statistics from "./components/Statistics";
-
-// function App() {
-//   const [count, setCount] = useState(0);
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more if you dare! That's
-//         incredible...
-//       </p>
-//     </>
-//   );
-// }
-
-// export default App;
 
 export default function App() {
   const [screen, setScreen] = React.useState("mainMenu");
--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -47,20 +47,6 @@ export default function MainMenu(props) {
         src="./src/assets/main_menu/floaty_house.png"
         className="floaty_house"
       />
-      {/* <img src="./src/assets/logo/logo.png" /> */}
-      {/* <img
-        src="./src/assets/main_menu/title_castle.png"
-        onClick={() => props.setScreen("characterSelect")}
-        className="castle"
-      />
-      <img
-        src="./src/assets/main_menu/title_library.png"
-        onClick={() => {
-          props.setScreen("game-memory");
-          clickAudio.play();
-        }}
-        className="library"
-      /> */}
     </div>
   );
 }
